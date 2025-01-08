085220b1886d69611bcfe14b45318c1d7384737c
Sabina Bahodirova <bahodirova2000@gmail.com>
2025-01-08 14:43:58 -0500

    Babel not working

--- Passenger/app/(auth)/welcome.tsx
+++ Passenger/app/(auth)/welcome.tsx
@@ -4,7 +4,7 @@ import { SafeAreaView } from 'react-native-safe-area-context'
 
 const Onboarding = () => {
     return (
-        <SafeAreaView style={{ flex: 1, justifyContent: 'center', alignItems: 'center', backgroundColor: 'white' }}>
+        <SafeAreaView className="flex h-full items-center justify-between bg-white">
             <Text>Onboarding</Text>
         </SafeAreaView>
 
