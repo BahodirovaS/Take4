514a178bb5cc0ccf42881e6bd01b4c9109c746a1
Sabina Bahodirova <bahodirova2000@gmail.com>
2025-04-17 13:51:10 -0700

    normal font

--- Passenger/app/(root)/chat.tsx
+++ Passenger/app/(root)/chat.tsx
@@ -292,7 +292,7 @@ const styles = StyleSheet.create({
         borderColor: "#ddd",
         backgroundColor: "#f4f4f4",
         fontSize: 18,
-        fontFamily: "DMSans-Medium",
+        fontFamily: "DMSans",
     },
     sendButton: {
         paddingVertical: 12,
